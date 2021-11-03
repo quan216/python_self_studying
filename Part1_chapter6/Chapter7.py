99a7e7e7776492feb610e4b7997455202f05c056
quan216 <92570237+quan216@users.noreply.github.com>
2021-11-03 23:21:22 +0900

    them code nhung chua chay duowc

--- Part1_chapter6/Chapter7.py
+++ Part1_chapter6/Chapter7.py
@@ -2,12 +2,25 @@
 a = []
 
 for i in range(25,51):
-    a = a.reverse(i)
+    #a = a.reverse(i)
     i += 1
 
 print(a)
 
 #Excercie 3
 
+while True:
+    input = input("your number, or q to quit game: ")
+    if input == "q":
+            print("quit game!")
+            break
+    try:
+        input = int(input)
+    except ValueError:
+        print("please type a number or q to quit.")
 
+        if input in range(25,50):
+            print("you win! congrast!")
+        else:
+            print("again")
 

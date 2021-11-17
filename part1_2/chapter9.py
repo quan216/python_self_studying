6e37a9c35344b75944e9e322430cfe352fe52d5b
quan216 <92570237+quan216@users.noreply.github.com>
2021-11-17 23:19:30 +0900

    kho that day van deo duoc

--- part1_2/chapter9.py
+++ part1_2/chapter9.py
@@ -1,5 +1,14 @@
 #bat dau code voi filegit
+import csv
+with open("testfile.csv", "r") as f:
+    print(f.read())
+
+ten = input("ten ban la cai lon gi: ")
+age = input("tuoi la bao nhieu: ")
+answer = f'{ten}, {age}\n'
+
 
 with open("testfile.csv", "w", encoding="UTF-8", newline="") as f:
-    f.write("helo git!")
-    
+    f.write(answer)
+
+

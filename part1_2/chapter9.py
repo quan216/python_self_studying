0b94fafbbec862ee6efab9e570edbf64575b9c40
quan216 <92570237+quan216@users.noreply.github.com>
2021-11-17 23:44:06 +0900

    tam dung bt2 ghi file 2021 11 17

--- part1_2/chapter9.py
+++ part1_2/chapter9.py
@@ -1,14 +1,22 @@
 #bat dau code voi filegit
 import csv
+from os import name
 with open("testfile.csv", "r") as f:
     print(f.read())
-
-ten = input("ten ban la cai lon gi: ")
-age = input("tuoi la bao nhieu: ")
-answer = f'{ten}, {age}\n'
+data_total = []
 
 
-with open("testfile.csv", "w", encoding="UTF-8", newline="") as f:
-    f.write(answer)
+for i in range(0,2):
+
+    name = input("ten ban la cai lon gi: ")
+    age = input("tuoi la bao nhieu: ")
+    data_total = f"{name}, {age}, \n"
+    print(data_total)
+
+    with open("testfile.csv", "a", encoding="UTF-8", newline="") as f:
+        # w = csv.writer(f, delimiter = ",")
+        # w.writerow(data_total)
+        f.write(data_total)
+        
 
 

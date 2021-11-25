e075f8b058970856467c0ef0eab9fbc2798821d2
quan216 <92570237+quan216@users.noreply.github.com>
2021-11-25 16:49:32 +0900

    bt 3 class

--- part1_2/chapter12_2.py
+++ part1_2/chapter12_2.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Circle:
     def __init__(self, r):
         self.radius = r
@@ -9,10 +8,42 @@ class Circle:
     def area(self):
        return self.radius ** 2 * math.pi
 
-       
 
-A_cir = Circle(14)
-print(A_cir.radius)
-print(A_cir.area())
+
+class Triangle:
+    def __init__(self, d1, d2, d3):
+        self.dimension1 = d1
+        self.dimension2 = d2
+        self.dimension3 = d3
+        self.S = (self.dimension1 + self.dimension2 + self.dimension3)/2
+
+    def area_cal(self):
+        return math.sqrt(self.S*(self.S- self.dimension1)*\
+            (self.S - self.dimension2)*(self.S - self.dimension3))
+
+
+
+
+
+print(" 3 canh cua tam giac (a, b, c): ")
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+
+if abs(b-c) < a and a < a+b :
+    t_triangle = Triangle(a, b, c)
+    print("area of trigangle is ")
+    print(t_triangle.area_cal())
+else: 
+    print("these dimensions are not become a triangle ")
+
+
+
+
+
+
+
+
+    
 
 

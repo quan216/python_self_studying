fbaabe5dda322fcc67ffeeb6987a9fc26777f7a7
quan216 <92570237+quan216@users.noreply.github.com>
2021-11-26 11:08:38 +0900

    xong bt 1 to 3

--- part1_2/chapter13_1.py
+++ part1_2/chapter13_1.py
@@ -1,7 +1,11 @@
 ##
 import math
 
-class Rectange:
+class Shape:
+    def what_am_i(self):
+        print("I am a shape")
+
+class Rectange(Shape):
     def __init__(self, d1, d2):
         self.dimension1 = d1
         self.dimension2 = d2
@@ -13,18 +17,30 @@ class Rectange:
 class Square(Rectange):
     def cal_are(self):
         return self.dimension1 * self.dimension2
+    def change_size(self, d1, d2):
+        self.dimension1 = d1
+        self.dimension2 = d2
+
+
+
+
 
 
 
 
 rect_a = Rectange(20,15)
+rect_a.what_am_i()
 
 print(rect_a.cal_Perimeter())
-
 rect_b = Square(50, 49)
 print(rect_b.cal_Perimeter())
 
 print(rect_b.cal_are())
+
+rect_b.change_size(100, 50)
+print(rect_b.cal_Perimeter())
+print(rect_b.cal_are())
+
 
 
 
